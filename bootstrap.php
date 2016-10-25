b625f28dc69a1e08c2d11c3443a9a356e595c84f
Witold Adamus <witold.adamus@xsolve.pl>
2016-10-25 18:15:47 +0200

    Code coverage check for at least 50%

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,5 @@
 <?php
-if ( ! is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+if ( ! is_file($autoloadFile = __DIR__ . '/vendor/autoload.php')) {
     echo 'Could not find "vendor/autoload.php". Did you forget to run "composer install --dev"?'.PHP_EOL;
     exit(1);
 }
